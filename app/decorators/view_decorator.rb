c94f59efcb0423f171cd90328d39a389d506c151
Jiří Čech <jiricech94@gmail.com>
2022-11-01 00:27:14 +0100

    Remove redundant application views

--- app/decorators/view_decorator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ViewDecorator
-  include ViewDecorator::ControllerDecorator
-  include ViewDecorator::ModelDecorator
-  include ViewDecorator::PathDecorator
-  include ViewDecorator::ResourceDecorator
-end
