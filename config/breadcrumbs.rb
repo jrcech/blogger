2922fcb5638af7045d5d6f6e98e10cb85f125920
Jiří Čech <jiricech94@gmail.com>
2022-11-15 21:48:59 +0100

    Refactor methods in breadcrumbs

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -4,15 +4,15 @@ crumb :root do
   link t('controller.dashboard'), admin_root_path
 end
 
-crumb :items do |model_symbol|
+crumb :items do |model_symbol_plural|
   link(
     render(
       Tooltips::TooltipComponent.new(
-        icon: model_icon(model_symbol),
-        title: t("models.#{model_symbol}.more")
+        icon: model_icon(model_symbol_plural),
+        title: t("models.#{model_symbol_plural}.more")
       )
     ),
-    send("admin_#{model_symbol}_path")
+    send("admin_#{model_symbol_plural}_path")
   )
 end
 
@@ -22,34 +22,16 @@ crumb :item do |item, item_presenter|
   parent :items, item_presenter.model_symbol_plural
 end
 
-crumb :new do |model_symbol|
-  link t('actions.new', item: model_singular), send("new_admin_#{model_singular_symbol}_path")
+crumb :new do |model_symbol_plural|
+  link t('actions.new', item: model_singular), send("new_admin_#{model_symbol_plural.to_s.singularize}_path")
 
-  parent :items, model_symbol
+  parent :items, model_symbol_plural
 end
 
 crumb :edit do |item, item_presenter|
   link t('actions.edit'), send("edit_admin_#{item_presenter.model_symbol}_path", id: item.id)
 
   parent :item, item, item_presenter
-end
-
-crumb :articles do
-  link(
-    render(
-      Tooltips::TooltipComponent.new(
-        icon: model_icon('articles'),
-        title: t("models.articles.more")
-      )
-    ),
-    admin_articles_path
-  )
-end
-
-crumb :article do |article, article_presenter|
-  link article_presenter.title, admin_article_path(article)
-
-  parent :articles
 end
 
 crumb :reviews do |article, article_presenter|
@@ -65,7 +47,7 @@ crumb :reviews do |article, article_presenter|
     )
 
 
-    parent :article, article, article_presenter
+    parent :item, article, article_presenter
   else
     link(
       render(
