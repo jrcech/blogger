6b9c8860904fbbc3f029da4bbca1e6d56d6f0985
Jiří Čech <jiricech94@gmail.com>
2023-03-23 16:30:51 +0100

    Add terraform for deployment

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.4.2"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
+
+resource "digitalocean_droplet" "blogger" {
+  image = "ubuntu-22-04-x64"
+  name = "blogger"
+  region = "fra1"
+  size = "s-1vcpu-512mb-10gb"
+  ssh_keys = ["ed:b5:ed:cb:c7:54:2b:1c:58:3a:22:5b:28:c8:a0:87"]
+  user_data = file("./blogger_app.yaml")
+}
