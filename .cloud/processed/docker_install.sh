626fa2d6ff7651f946fb529d1cf00e694cf4c39b
Jiří Čech <jiricech94@gmail.com>
2024-05-19 23:43:33 +0200

    Delete redundant files

--- .cloud/processed/docker_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt update && apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update && sudo apt install -q -y docker-ce
-sudo usermod -aG docker jrcech
--- .cloud/processed/prezto_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
-
-git clone https://github.com/jrcech/vps_dotfiles ~/.dotfiles
-
-~/.dotfiles/install.sh
